2f8395a50e0ffbfe956c0633a0a393c9c8de1d58
Brett Cooper <brett@thewiznerd.com>
2023-04-08 15:27:21 -0600

    Working on auto imports.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
+  plugins: [
+    AutoImport({
+      dts: true,
+      include: [
+        /\.ts$/
+      ],
+      dirs: [
+        './src/constants',
+        './src/entities',
+        '../other/super-simple-js-game-engine/src',
+        '../other/super-simple-js-game-engine/src/composition',
+      ]
+    })
+  ],
   server: {
     hmr: {
       path: '/hmr',

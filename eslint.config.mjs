41efe8154d00c0a45d782b1e3a598022c9b1b16a
Brett Cooper <brett@thewiznerd.com>
2025-03-01 15:04:04 -0700

    Added linting.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,6 @@
 // @ts-check
 
-import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-);
+export default tseslint.config(prettierRecommended);
--- vite.config.prod.mjs
+++ vite.config.prod.mjs
@@ -1,28 +1,28 @@
-import { defineConfig } from 'vite';  
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './',
-    logLevel: 'warning',
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    phaser: ['phaser']
-                }
-            }
+  base: './',
+  logLevel: 'warning',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ['phaser'],
         },
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                passes: 2
-            },
-            mangle: true,
-            format: {
-                comments: false
-            }
-        }
+      },
     },
-    server: {
-        port: 8080
-    }
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        passes: 2,
+      },
+      mangle: true,
+      format: {
+        comments: false,
+      },
+    },
+  },
+  server: {
+    port: 8080,
+  },
 });
